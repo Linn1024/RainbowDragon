c293ab46b78e664ede4f3f3f3b0fe14891b64d99
Linn1024 <linn1024@gmail.com>
2019-11-09 01:53:58 +0300

    Refactor

--- problems/1/gen.py
+++ problems/1/gen.py
@@ -10,14 +10,13 @@ rdDelimeterTwo = ''
 macroses = {}
 upStr = ''
 	
-def makeState(name, filetex):
+def makeState(name, curProb, userPathProb):
 	global macroses, upStr
+	makeTests(name, curProb, userPathProb)
 	makeVars(name)
 #	print(f"Name: {name}, Upstr : {upStr}")
-	stateTemp = open(filetex + '/statements.tex', encoding="UTF-8").readlines()
-	probTemp = open(filetex + '/problem.tex', encoding="UTF-8").readlines()
-
-
+	stateTemp = open(f"{curProb['path']}/statements.tex", encoding="UTF-8").readlines()
+	probTemp = open(f"{curProb['path']}/problem.tex", encoding="UTF-8").readlines()
 	macroses['rdStudent'] = name
 	macroses['rdName'] = "Считывание"
 	rdFirstVarString = { 'int' : 'целое число n $(1 \\le n \\le 10^{10})$', 'char' : 'символ'}
@@ -31,24 +30,36 @@ def makeState(name, filetex):
 	macrString = []
 	for i in macroses:
 		macrString.append('\\newcommand{\\' + i + '}{' + macroses[i] + '}\n')
-	return [probTemp, macrString + stateTemp]
+	
+	resProb, resState = probTemp, macrString + stateTemp
+	probFile = open(f"{userPathProb}/problem.tex", 'w', encoding="UTF-8")
+	stateFile = open(f"{userPathProb}/statement.tex", 'w', encoding="UTF-8")
+	print("".join(resProb), file=probFile)
+	print("".join(resState), file=stateFile)
+	probFile.close()
+	stateFile.close()
+	os.chdir(f"{userPathProb}")
+#	print(" ".join(['cd', f"{userPathProb}", "&&", "pdflatex", "statement.tex"]))
+	subprocess.run(["pdflatex", "statement.tex"])
+	os.chdir('../../../')
+	return True
 
-def makeVars(name):	
+
+def makeVars(name):
 	global rdFirstVar, rdSecondVar, rdDelimeterOne, rdDelimeterTwo, upStr
-	rdFirstVar = rdFirstVarChose[hashStr(upStr, 31, 2)]         
-	rdSecondVar = rdSecondVarChose[hashStr(upStr, 37, 2)]       
-	rdDelimeterOne = rdDelimeterChose[hashStr(upStr, 41, 3)]    
+	rdFirstVar = rdFirstVarChose[hashStr(upStr, 31, 2)]
+	rdSecondVar = rdSecondVarChose[hashStr(upStr, 37, 2)]
+	rdDelimeterOne = rdDelimeterChose[hashStr(upStr, 41, 3)]
 	rdDelimeterTwo = rdDelimeterChose[hashStr(upStr, 41, 3) - 1]
-	print(f"Tests:         {rdFirstVar}    {rdSecondVar}")
 
 def replaceSol(matchobj):
 	return rdDelimeterOne if matchobj.groups()[0] == 'rdDelimeterOne' else rdDelimeterTwo
 
-def makeSol(name, pathProb, pathUser):
+def makeSol(name, curProb, pathUser):
 	import re
 	rep = re.compile("@(.*?)@")
-	sol = open(pathProb + 'sol.py').readlines()
-	solution = open(pathUser + 'solution.py', 'w')
+	sol = open(f"{curProb['path']}/sol.py").readlines()
+	solution = open(f"{pathUser}/solution.py", 'w')
 	for i in sol:
 		print(re.sub(rep, replaceSol, i), file=solution)	
 		
@@ -61,23 +72,30 @@ def hashStr(s, p, MOD):
 	return h
 
 
-num = 1
-def makeTests(name, pathProb, pathUser):
+num = 0
+def makeTests(name, curProb, pathUser):
 	global num, upStr
+	try:
+		os.mkdir(f"{pathUser}/tests")
+	except Exception as e:
+		num = len(os.listdir(f"{pathUser}/tests")) // 2
+		print(f"NUM :::::::::::: num")
+		print(e)
+		return
 	upStr = name.upper()
-	num = 1
+	num = 0
 	makeVars(name)
-	makeSol(name, pathProb, pathUser)
+	makeSol(name, curProb, pathUser)
 	from random import randint, random, seed
 #	print(f"Upstr : {upStr}")
 	seed(hashStr(upStr, 31, 20))
 	def printTest(test):
 		global num
-		testFile = open(pathUser + 'tests/' + str(num), 'w')
+		testFile = open(pathUser + '/tests/' + str(num), 'w')
 		print(test, file=testFile)
 		testFile.close()
-		print('python' + ' "' + pathUser + 'solution.py" <"' + pathUser + 'tests/' + str(num) + '">"' + pathUser + 'tests/' + str(num) + '.a"')
-		os.system('python' + ' "' + pathUser + 'solution.py" <"' + pathUser + 'tests/' + str(num) + '">"' + pathUser + 'tests/' + str(num) + '.a"')
+		print('python' + ' "' + pathUser + '/solution.py" <"' + pathUser + '/tests/' + str(num) + '">"' + pathUser + '/tests/' + str(num) + '.a"')
+		subprocess.run(["python", f"{pathUser}/solution.py"], stdin=open(f"{pathUser}/tests/{num}"), stdout=open(f"{pathUser}/tests/{num}.a", "w"))
 		num += 1
 	def randStr(n):
 		ls = []
@@ -103,14 +121,16 @@ def makeTests(name, pathProb, pathUser):
 		printTest(rdTestOne() + rdDelimeterOne + rdTestTwo())
 
 
-def checkSol(pathProb, pathUser):
+def checkSol(name, curProb, pathUser):
 	global num
+	makeTests(name, curProb, pathUser)
 	os.chdir(f"{pathUser}/files")
 	print(os.getcwd())
 #	os.system("g++ check.cpp -o check")
 	subprocess.run(['g++', 'check.cpp', '-o', 'check'])
 	subprocess.run(['gcc', '-x', 'c', 'solution', '-o', 'sol'])
-	for i in range(1, num + 1):
+	print(f"NUM :       {num}")
+	for i in range(0, num):
 		os.system(f"sol < ../tests/{i} > out")
 		res = subprocess.run(['check', f'../tests/{i}', f'out', f'../tests/{i}.a'], capture_output=True)
 		if res.returncode != 0:
--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 ﻿import sys
 import subprocess
 import os
-from shutil import copyfile, rmtree
+from shutil import copyfile, rmtree, copy
 
 userName = ""
 listOfProbs = []
@@ -31,14 +31,13 @@ def findAllProblems():
 	global listOfProbs
 	listOfProbsFiles = next(os.walk("problems"))[1]
 	for prob in listOfProbsFiles:
-		exists = os.path.isfile('problems/' + prob + '/meta')
+		exists = os.path.isfile(f'problems/{prob}/meta')
 		if exists:
-			p = parseMeta('problems/' + prob + '/meta')
-			p['path'] = 'problems/' + prob + '/'
+			p = parseMeta(f'problems/{prob}/meta')
+			p['path'] = f'problems/{prob}'
 			print(p.props)
 			listOfProbs.append(p)
 	    	
-
 
 def defineUser():
 	global userName
@@ -63,11 +62,11 @@ def changeUserName(nameEntry):
 
 def makeDir():
 	try:
-		os.mkdir('Users/' + userName)
+		os.mkdir(f'Users/{userName}')
 	except Exception as e:
 		print(e)
 	try:
-		os.mkdir('Users/' + userName + '/' + curProbName.get())
+		os.mkdir(f'Users/{userName}/{curProbName.get()}')
 	except Exception as e:
 		print(e)
 
@@ -79,61 +78,66 @@ def curProbFind():
 
 def makeTests():
 	global curProbName
-	makeDir()
-	curProbFind()
-	userPathProb = 'Users/' + userName + '/' + curProbName.get() + '/'	
-	sys.path.insert(0, curProb['path'])
-	import gen
+	userPathProb = initDir()
 	try:
-		os.mkdir(userPathProb + 'tests')
-		gen.makeTests(userName.strip(), curProb['path'], userPathProb)
+		gen.makeTests(userName.strip(), curProb, userPathProb)
 	except Exception as e:
 		print(e)
 
-def showStatement():
-	global curProbName
+def copyInitFiles(userPathProb):
+	src_files = os.listdir("commonFiles")
+	for file_name in src_files:
+		full_file_name = os.path.join("commonFiles", file_name)
+		copy(full_file_name, userPathProb)
+
+def initDir():
 	makeDir()
 	curProbFind()
-	userPathProb = 'Users/' + userName + '/' + curProbName.get() + '/'
-	exists = os.path.isfile('' + userPathProb + 'statement.pdf')
-	print(exists)
-	if exists:
-		os.system('"' + userPathProb + 'statement.pdf"')
-		return
-	makeTests()
+	userPathProb = f'Users/{userName}/{curProbName.get()}'
+	sys.path.insert(0, curProb['path'])
 	import gen
-	resProb, resState = gen.makeState(userName.strip(), curProb['path'])
-	probFile = open(userPathProb + 'problem.tex', 'w', encoding="UTF-8")
-	stateFile = open(userPathProb + 'statement.tex', 'w', encoding="UTF-8")
-	print(*resProb, file=probFile)
-	print(*resState, file=stateFile)
-	copyfile("makeStatement/task.sty", userPathProb + 'task.sty')
-	probFile.close()
-	stateFile.close()
-	print('pdflatex "' + userPathProb + 'statement.tex"')
-	os.system('cd "' + userPathProb + '" && pdflatex statement.tex')
-	os.system('"' + userPathProb + 'statement.pdf"')
-	
+	copyInitFiles(userPathProb)
+	return userPathProb
+
+def showStatement():
+	global curProbName
+	userPathProb = initDir()
+	exists = os.path.isfile(f'{userPathProb}/statement.pdf')
+	print(f"Statement is exists : {exists}")
+	if exists:
+		os.system(f'"{userPathProb}/statement.pdf"')
+		return
+	import gen
+	if gen.makeState(userName.strip(), curProb, userPathProb):	
+		os.system(f'"{userPathProb}/statement.pdf"')
+
 def defineSubmitFileName(name, entry):
 	entry.delete(0, END)
 	entry.insert(0, askopenfilename())
 
 def checkSol(sol, logsText):
-	makeDir()
-	curProbFind()
-	makeTests()
-	userPathProb = 'Users/' + userName + '/' + curProbName.get()
+	import time 
+	logsText.config(state=NORMAL)
+	logsText.delete("1.0", END)
+	logsText.insert(END, "Solution is checking...")
+	logsText.config(state=DISABLED)
+	master.update()
+	userPathProb = initDir()
 	os.mkdir(f"{userPathProb}/files")	
 	for i in curProb['files'].split(','):
-		copyfile(f"{curProb['path']}{i}", f"{userPathProb}/files/{i}")
+		copyfile(f"{curProb['path']}/{i}", f"{userPathProb}/files/{i}")
 	copyfile(sol, f"{userPathProb}/files/solution")
 	import gen
 	logsText.config(state=NORMAL)
 	logsText.delete("1.0", END)
-	logsText.insert(END, gen.checkSol(curProb['path'], userPathProb))
+	logsText.insert(END, gen.checkSol(userName, curProb, userPathProb))
 	logsText.config(state=DISABLED)
 	rmtree(f'{userPathProb}/files')
-		
+
+def delProbFiles():
+	curProbFind()
+	userPathProb = f'Users/{userName}/{curProbName.get()}'
+	rmtree(f"{userPathProb}")	
 
 def makeAllFields():
 	global master, curProbName, submitFileName
@@ -151,6 +155,10 @@ def makeAllFields():
 	optionMenuProblems.grid(row=0, column=3)
 	showStatementButton = Button(master, text="Show statement", command=showStatement)
 	showStatementButton.grid(row=1, column=1)
+	deleteFilesButton = Button(master, text="Delete prob files", command=delProbFiles)
+	deleteFilesButton.grid(row=1, column=3)
+
+
 	chooseFileButton = Button(master, text="Choose File", command=lambda : defineSubmitFileName(submitFileName, chooseFileEntry))
 	chooseFileButton.grid(row=2, column=0)
 	chooseFileEntry = Entry(master, width=50)
